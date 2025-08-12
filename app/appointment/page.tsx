3bcd8f072f37663bcfe0fc9f86a27820c927bc06
talhaadil48 <talhaadil48@icloud.com>
2025-08-12 18:38:53 +0500

    hello

--- app/appointment/page.tsx
+++ app/appointment/page.tsx
@@ -1,28 +1,10 @@
-"use client"
 
-import { useEffect } from 'react'
 import AppointmentScheduler from "@/components/Essentials/BookAppointment"
 import VerticalHeroSlider from "@/components/Essentials/VerticalBanner"
 
 export default function Page() {
 
-  useEffect(() => {
-    async function fetchIp() {
-      try {
-        const res = await fetch('/api/get-ip')
-        const data = await res.json()
-        console.log("User IP:", data.ip)
-        const result = await fetch(`https://ip-api.com/json/${data.ip}`)
-        const dataresult = await result.json()
-        console.log('User country:',dataresult)
 
-      } catch (error) {
-        console.error("Error fetching IP:", error)
-      }
-    }
-
-    fetchIp()
-  }, [])
 
   return (
     <div>
